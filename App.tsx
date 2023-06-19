d3950236170a2b4f5b348e0d02d7ab4ebe5cd52b
Dirlan Ferreira <dirlan2014@yahoo.com.br>
2023-06-19 11:58:50 -0300

    feat: added header & logo component

--- App.tsx
+++ App.tsx
@@ -1,13 +1,21 @@
+import Loading from "@components/Loading";
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
 import Groups from "@screens/Groups";
 import theme from "@theme/index";
 import { StatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components/native";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
       <StatusBar style="light" translucent />
-      <Groups />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,10 @@
-import { Text, View } from "react-native";
+import Header from "@components/Header";
 import { Container } from "./styles";
 
 export default function Groups() {
   return (
     <Container>
-      <Text>Groups</Text>
+      <Header />
     </Container>
   );
 }

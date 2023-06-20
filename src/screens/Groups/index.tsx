eacfca575d53e0a1f4471504dd6a496f7adf76b3
Dirlan Ferreira <dirlan2014@yahoo.com.br>
2023-06-20 10:38:51 -0300

    feat: added groupcard component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,14 @@
 import Header from "@components/Header";
 import { Container } from "./styles";
 import Highlight from "@components/Highlight";
+import GroupCard from "@components/GroupCard";
 
 export default function Groups() {
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subTitle="Jogue com sua turma" />
+      <GroupCard title="teste" />
     </Container>
   );
 }

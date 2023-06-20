cfb8894df0aefca0883a8144ea863dd1bd647ea4
Dirlan Ferreira <dirlan2014@yahoo.com.br>
2023-06-20 11:02:54 -0300

    feat: added listempty component

---
+++ src/components/ListEmpty/index.tsx
@@ -0,0 +1,9 @@
+import { Container, Message } from "./styles";
+
+export default function ListEmpty() {
+  return (
+    <Container>
+      <Message>Empty List</Message>
+    </Container>
+  );
+}
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,9 +4,10 @@ import Highlight from "@components/Highlight";
 import GroupCard from "@components/GroupCard";
 import { useState } from "react";
 import { FlatList } from "react-native";
+import ListEmpty from "@components/ListEmpty";
 
 export default function Groups() {
-  const [groups, setGroups] = useState<string[]>(["Group 1", "Group 2"]);
+  const [groups, setGroups] = useState<string[]>([]);
 
   return (
     <Container>
@@ -16,6 +17,8 @@ export default function Groups() {
         data={groups}
         keyExtractor={(item) => item}
         renderItem={({ item }) => <GroupCard title={item} />}
+        ListEmptyComponent={() => <ListEmpty />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
       />
     </Container>
   );

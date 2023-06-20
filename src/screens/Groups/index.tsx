003846e346df92c5b52af75418c9bc94ba931ca3
Dirlan Ferreira <dirlan2014@yahoo.com.br>
2023-06-20 10:54:08 -0300

    feat: added groupcard to flatlist

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,13 +2,21 @@ import Header from "@components/Header";
 import { Container } from "./styles";
 import Highlight from "@components/Highlight";
 import GroupCard from "@components/GroupCard";
+import { useState } from "react";
+import { FlatList } from "react-native";
 
 export default function Groups() {
+  const [groups, setGroups] = useState<string[]>(["Group 1", "Group 2"]);
+
   return (
     <Container>
       <Header />
-      <Highlight title="Turmas" subTitle="Jogue com sua turma" />
-      <GroupCard title="teste" />
+      <Highlight title="Teams" subTitle="Play with your team" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   );
 }
